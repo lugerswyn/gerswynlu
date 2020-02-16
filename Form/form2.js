31c51dcb83f34484a2b0e1280aa28e95ee427f2b
lugerswyn <gerswynauldric.lu.iics@ust.edu.ph>
2020-02-16 23:17:17 +0800

    third Proj

---
+++ Form/form2.js
@@ -0,0 +1,19 @@
+function sReq(){
+    alert("You have sent a request");
+}
+
+function redo(){
+    prompt.alert("cleared");
+}
+
+function valiDate{
+    var start = new Date(dateF);
+    var end = new Date(dateL);
+}
+
+if(start>end){
+    alert("valid");
+}
+else{
+    alert("invalid");
+}
